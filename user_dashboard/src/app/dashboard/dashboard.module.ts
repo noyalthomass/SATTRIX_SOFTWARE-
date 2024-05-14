6b11ed3f32c73ab72754d9377b8a3e027e01491b
noyalthomass <noyallthomas@gmail.com>
2024-05-14 07:44:31 -0700

    feat:intial component structure,routing and modules lazy loaded

--- user_dashboard/src/app/dashboard/dashboard.module.ts
+++ user_dashboard/src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidepanelComponent } from './components/sidepanel/sidepanel.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ListComponent } from './components/list/list.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', component: RegisterComponent }, 
+  { path: 'list', component: ListComponent }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidepanelComponent,
+    RegisterComponent,
+    ListComponent,
+    DashboardComponent
+  ],
   imports: [
     CommonModule
   ]
--- user_dashboard/src/app/login/login.module.ts
+++ user_dashboard/src/app/login/login.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './pages/login/login.component';
+import { LoginAndRegisterComponent } from './components/login-and-register/login-and-register.component';
+import { Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', component: LoginComponent } 
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    LoginAndRegisterComponent
+  ],
   imports: [
     CommonModule
   ]
